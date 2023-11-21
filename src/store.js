6e9c7e9c161f4409c1c9797824be23be3957f6aa
elmiramm <elmira.m.m@yandex.ru>
2023-11-22 02:27:19 +0300

    task2

--- src/store.js
+++ src/store.js
@@ -2,78 +2,82 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
+	constructor(initState = {}) {
+		this.state = initState;
+		this.listeners = []; // Слушатели изменений состояния
+		this.counter = initState.list.length;
+	}
 
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
-  }
+	/**
+	 * Подписка слушателя на изменения состояния
+	 * @param listener {Function}
+	 * @returns {Function} Функция отписки
+	 */
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Возвращается функция для удаления добавленного слушателя
+		return () => {
+			this.listeners = this.listeners.filter(item => item !== listener);
+		}
+	}
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+	/**
+	 * Выбор состояния
+	 * @returns {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
+	/**
+	 * Установка состояния
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Вызываем всех слушателей
+		for (const listener of this.listeners) listener();
+	}
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+	/**
+	 * Добавление новой записи
+	 */
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+	addItem() {
+		this.counter = this.counter + 1;
+		this.setState({
+			...this.state,
+			list: [...this.state.list, { code: this.counter, title: 'Новая запись' }]
+		});
 
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    })
-  }
+	};
+
+	/**
+	 * Удаление записи по коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.filter(item => item.code !== code)
+		})
+	};
+
+	/**
+	 * Выделение записи по коду
+	 * @param code
+	 */
+	selectItem(code) {
+		this.setState({
+			...this.state,
+			list: this.state.list.map(item => {
+				if (item.code === code) {
+					item.selected = !item.selected;
+				}
+				return item;
+			})
+		})
+	}
 }
 
 export default Store;
